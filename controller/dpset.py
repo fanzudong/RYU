033e142b41fd47cd5d32577f37775a65b37ea005
fanzudong <1179528188@qq.com>
2017-05-05 17:30:24 +0800

    Update dpset.py

--- controller/dpset.py
+++ controller/dpset.py
@@ -17,7 +17,7 @@
 """
 Manage switches.
 
-Planned to be replaced by ryu/topology.
+计划由ryu/topology替代
 """
 
 import logging
@@ -36,106 +36,106 @@ LOG = logging.getLogger('ryu.controller.dpset')
 
 DPSET_EV_DISPATCHER = "dpset"
 
-
+#事件datapath基类
 class EventDPBase(event.EventBase):
     def __init__(self, dp):
         super(EventDPBase, self).__init__()
         self.dp = dp
 
-
+#事件datapath类
 class EventDP(EventDPBase):
     """
-    An event class to notify connect/disconnect of a switch.
+    用于通知交换机的连接/断开的事件类。
 
-    For OpenFlow switches, one can get the same notification by observing
-    ryu.controller.ofp_event.EventOFPStateChange.
-    An instance has at least the following attributes.
+    对于OpenFlow交换机，可以通过观察获得相同的通知
+    ryu.controller.ofp_event.EventOFPStateChange。
+    实例至少具有以下属性。
 
     ========= =================================================================
-    Attribute Description
+    属性      描述
     ========= =================================================================
-    dp        A ryu.controller.controller.Datapath instance of the switch
-    enter     True when the switch connected to our controller.  False for
-              disconnect.
-    ports     A list of port instances.
+    dp        一个 ryu.controller.controller.Datapath instance of the switch
+    enter     当交换机连接到我们的控制器时为真。 
+              假的时候断开。
+    ports     端口实例化列表.
     ========= =================================================================
     """
-
+#初始化
     def __init__(self, dp, enter_leave):
-        # enter_leave
-        # True: dp entered
-        # False: dp leaving
+        # 进入_离开
+        # True: dp 进入
+        # False: dp 离开
         super(EventDP, self).__init__(dp)
         self.enter = enter_leave
-        self.ports = []  # port list when enter or leave
+        self.ports = []  # 何时进入和离开的端口列表
 
-
+#事件datapath重连接
 class EventDPReconnected(EventDPBase):
     def __init__(self, dp):
         super(EventDPReconnected, self).__init__(dp)
         # port list, which should not change across reconnects
         self.ports = []
 
-
+#事件端口基类
 class EventPortBase(EventDPBase):
     def __init__(self, dp, port):
         super(EventPortBase, self).__init__(dp)
         self.port = port
 
-
+#事件新增端口
 class EventPortAdd(EventPortBase):
     """
-    An event class for switch port status "ADD" notification.
+    交换机端口状态“ADD”通知的事件类.
 
-    This event is generated when a new port is added to a switch.
-    For OpenFlow switches, one can get the same notification by observing
+    当新端口添加到交换机时，会生成此事件.
+    对于OpenFlow交换机，可以获得相同的通知，通过观察
     ryu.controller.ofp_event.EventOFPPortStatus.
-    An instance has at least the following attributes.
+    实例至少具有以下属性.
 
     ========= =================================================================
-    Attribute Description
+    属性      描述
     ========= =================================================================
     dp        A ryu.controller.controller.Datapath instance of the switch
-    port      port number
+    port      端口数量
     ========= =================================================================
     """
-
+#初始化
     def __init__(self, dp, port):
         super(EventPortAdd, self).__init__(dp, port)
 
-
+#事件端口删除类
 class EventPortDelete(EventPortBase):
     """
-    An event class for switch port status "DELETE" notification.
+    用于切换端口状态“DELETE”通知的事件类.
 
-    This event is generated when a port is removed from a switch.
-    For OpenFlow switches, one can get the same notification by observing
+    当端口从交换机中删除时，会生成此事件。
+    对于OpenFlow交换机，可以获得相同的通知通过观察
     ryu.controller.ofp_event.EventOFPPortStatus.
-    An instance has at least the following attributes.
+    实例至少具有以下属性.
 
     ========= =================================================================
-    Attribute Description
+    属性      描述
     ========= =================================================================
     dp        A ryu.controller.controller.Datapath instance of the switch
-    port      port number
+    port      端口数量
     ========= =================================================================
     """
 
     def __init__(self, dp, port):
         super(EventPortDelete, self).__init__(dp, port)
 
-
+#事件端口修改类
 class EventPortModify(EventPortBase):
     """
-    An event class for switch port status "MODIFY" notification.
+    切换端口状态“MODIFY”通知的事件类.
 
-    This event is generated when some attribute of a port is changed.
-    For OpenFlow switches, one can get the same notification by observing
+    当端口的某些属性更改时，会生成此事件.
+    对于OpenFlow交换机，可以获得相同的通知通过观察
     ryu.controller.ofp_event.EventOFPPortStatus.
-    An instance has at least the following attributes.
+    实例至少具有以下属性.
 
     ========= ====================================================================
-    Attribute Description
+    属性      描述
     ========= ====================================================================
     dp        A ryu.controller.controller.Datapath instance of the switch
     port      port number
@@ -145,46 +145,46 @@ class EventPortModify(EventPortBase):
     def __init__(self, dp, new_port):
         super(EventPortModify, self).__init__(dp, new_port)
 
-
+#端口状态类
 class PortState(dict):
     def __init__(self):
         super(PortState, self).__init__()
-
+#增加
     def add(self, port_no, port):
         self[port_no] = port
-
+#移除
     def remove(self, port_no):
         del self[port_no]
-
+#修改
     def modify(self, port_no, port):
         self[port_no] = port
 
 
-# this depends on controller::Datapath and dispatchers in handler
+# 这取决于controller :: Datapath和调度程序在处理程序中
 class DPSet(app_manager.RyuApp):
     """
-    DPSet application manages a set of switches (datapaths)
-    connected to this controller.
+    DPSet应用程序管理一组交换机（datapaths）
+    连接到这个控制器。
     """
-
+#初始化
     def __init__(self, *args, **kwargs):
         super(DPSet, self).__init__()
         self.name = 'dpset'
 
         self.dps = {}   # datapath_id => class Datapath
         self.port_state = {}  # datapath_id => ports
-
+#注册者
     def _register(self, dp):
         LOG.debug('DPSET: register datapath %s', dp)
         assert dp.id is not None
 
-        # while dpid should be unique, we need to handle duplicates here
-        # because it's entirely possible for a switch to reconnect us
-        # before we notice the drop of the previous connection.
-        # in that case,
-        # - forget the older connection as it likely will disappear soon
-        # - do not send EventDP leave/enter events
-        # - keep the PortState for the dpid
+        # 不同于dpid是唯一的, 我们需要在这里重复处理
+        # 因为在我们注意到上一次连接的丢弃之前，
+        # 交换机完全可以重新连接。
+        # 在这种情况下,
+        # - 忘记旧的连接，因为它很可能会消失
+        # - 不要发送EventDP leave/enter events
+        # -保持dpid的端口状态
         send_dp_reconnected = False
         if dp.id in self.dps:
             self.logger.warning('DPSET: Multiple connections from %s',
@@ -207,7 +207,7 @@ class DPSet(app_manager.RyuApp):
             ev = EventDPReconnected(dp)
             ev.ports = self.port_state.get(dp.id, {}).values()
             self.send_event_to_observers(ev)
-
+#未注册者
     def _unregister(self, dp):
         # see the comment in _register().
         if dp not in self.dps.values():
@@ -215,8 +215,7 @@ class DPSet(app_manager.RyuApp):
         LOG.debug('DPSET: unregister datapath %s', dp)
         assert self.dps[dp.id] == dp
 
-        # Now datapath is already dead, so port status change event doesn't
-        # interfere us.
+        # 现在datapath已经dead，所以端口状态改变事件不会干扰我们.
         ev = EventDP(dp, False)
         for port in list(self.port_state.get(dp.id, {}).values()):
             self._port_deleted(dp, port)
@@ -226,32 +225,32 @@ class DPSet(app_manager.RyuApp):
 
         del self.dps[dp.id]
         del self.port_state[dp.id]
-
+#获取
     def get(self, dp_id):
         """
-        This method returns the ryu.controller.controller.Datapath
+        该方法返回 the ryu.controller.controller.Datapath
         instance for the given Datapath ID.
         """
         return self.dps.get(dp_id)
 
     def get_all(self):
         """
-        This method returns a list of tuples which represents
-        instances for switches connected to this controller.
-        The tuple consists of a Datapath Id and an instance of
-        ryu.controller.controller.Datapath.
-        A return value looks like the following:
+        此方法返回代表的元组列表
+        连接到该控制器的交换机的实例。
+        元组由数据路径标识和实例组成
+        ryu.controller.controller.Datapath。
+        返回值如下所示:
 
             [ (dpid_A, Datapath_A), (dpid_B, Datapath_B), ... ]
         """
         return list(self.dps.items())
-
+#已增加的端口
     def _port_added(self, datapath, port):
         self.port_state[datapath.id].add(port.port_no, port)
-
+#已删除的端口
     def _port_deleted(self, datapath, port):
         self.port_state[datapath.id].remove(port.port_no)
-
+#调度者改变
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [handler.MAIN_DISPATCHER, handler.DEAD_DISPATCHER])
     def dispatcher_change(self, ev):
@@ -261,7 +260,7 @@ class DPSet(app_manager.RyuApp):
             self._register(datapath)
         elif ev.state == handler.DEAD_DISPATCHER:
             self._unregister(datapath)
-
+#交换机特征处理者
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, handler.CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         msg = ev.msg
@@ -269,7 +268,7 @@ class DPSet(app_manager.RyuApp):
         # ofp_handler.py does the following so we could remove...
         if datapath.ofproto.OFP_VERSION < 0x04:
             datapath.ports = msg.ports
-
+#端口状态处理者
     @set_ev_cls(ofp_event.EventOFPPortStatus, handler.MAIN_DISPATCHER)
     def port_status_handler(self, ev):
         msg = ev.msg
@@ -297,13 +296,13 @@ class DPSet(app_manager.RyuApp):
                       dpid_to_str(datapath.id), port.port_no)
             self.port_state[datapath.id].modify(port.port_no, port)
             self.send_event_to_observers(EventPortModify(datapath, port))
-
+#获取端口
     def get_port(self, dpid, port_no):
         """
-        This method returns the ryu.controller.dpset.PortState
-        instance for the given Datapath ID and the port number.
-        Raises ryu_exc.PortNotFound if no such a datapath connected to
-        this controller or no such a port exists.
+        此方法返回ryu.controller.dpset.PortState
+        给定的Datapath ID和端口号的实例。
+        如果没有这样的数据路径连接到ryu_exc.PortNotFound
+        该控制器或没有这样的端口存在。
         """
         try:
             return self.port_state[dpid][port_no]
@@ -313,9 +312,9 @@ class DPSet(app_manager.RyuApp):
 
     def get_ports(self, dpid):
         """
-        This method returns a list of ryu.controller.dpset.PortState
-        instances for the given Datapath ID.
-        Raises KeyError if no such a datapath connected to this controller.
+        此方法返回ryu.controller.dpset.PortState的列表
+        给定Datapath ID的实例。
+        如果没有这样的数据路径连接到此控制器，则引发KeyError。
         """
         return list(self.port_state[dpid].values())
 
