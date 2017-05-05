f27da9c4833d52803836aed52bafc248a57bdeeb
fanzudong <1179528188@qq.com>
2017-05-05 15:30:50 +0800

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -15,10 +15,10 @@
 # limitations under the License.
 
 """
-The main component of OpenFlow controller.
+OpenFlow控制器的主要组件。
 
-- Handle connections from switches
-- Generate and route events to appropriate entities like Ryu applications
+- 处理交换机的连接
+- 生成并将事件路由到适当的实体，如Ryu应用程序
 
 """
 
@@ -47,45 +47,45 @@ from ryu.controller.handler import HANDSHAKE_DISPATCHER, DEAD_DISPATCHER
 from ryu.lib.dpid import dpid_to_str
 
 LOG = logging.getLogger('ryu.controller.controller')
-
+#缺省ofp主机
 DEFAULT_OFP_HOST = '0.0.0.0'
 
 CONF = cfg.CONF
 CONF.register_cli_opts([
     cfg.StrOpt('ofp-listen-host', default=DEFAULT_OFP_HOST,
-               help='openflow listen host (default %s)' % DEFAULT_OFP_HOST),
+               help='openflow listen host (default %s)' % DEFAULT_OFP_HOST),  #of监听主机
     cfg.IntOpt('ofp-tcp-listen-port', default=None,
-               help='openflow tcp listen port '
+               help='openflow tcp listen port '                               #of tcp监听端口
                     '(default: %d)' % ofproto_common.OFP_TCP_PORT),
     cfg.IntOpt('ofp-ssl-listen-port', default=None,
-               help='openflow ssl listen port '
+               help='openflow ssl listen port '                               #of ssl监听端口
                     '(default: %d)' % ofproto_common.OFP_SSL_PORT),
-    cfg.StrOpt('ctl-privkey', default=None, help='controller private key'),
-    cfg.StrOpt('ctl-cert', default=None, help='controller certificate'),
-    cfg.StrOpt('ca-certs', default=None, help='CA certificates')
+    cfg.StrOpt('ctl-privkey', default=None, help='controller private key'),   #控制器私有密钥
+    cfg.StrOpt('ctl-cert', default=None, help='controller certificate'),      #控制器证书
+    cfg.StrOpt('ca-certs', default=None, help='CA certificates')              #CA证书
 ])
 CONF.register_opts([
     cfg.FloatOpt('socket-timeout',
                  default=5.0,
-                 help='Time, in seconds, to await completion of socket operations.'),
+                 help='Time, in seconds, to await completion of socket operations.'), #时间，用秒计算，等待完成套接字操作
     cfg.FloatOpt('echo-request-interval',
                  default=15.0,
-                 help='Time, in seconds, between sending echo requests to a datapath.'),
+                 help='Time, in seconds, between sending echo requests to a datapath.'), #给datapath发送echo消息请求的时间间隔
     cfg.IntOpt('maximum-unreplied-echo-requests',
                default=0,
                min=0,
-               help='Maximum number of unreplied echo requests before datapath is disconnected.')
+               help='Maximum number of unreplied echo requests before datapath is disconnected.') #datapath未连接之前，echo请求消息最大未回复时间
 ])
 
-
+#of控制器类
 class OpenFlowController(object):
     def __init__(self):
         super(OpenFlowController, self).__init__()
         if not CONF.ofp_tcp_listen_port and not CONF.ofp_ssl_listen_port:
             self.ofp_tcp_listen_port = ofproto_common.OFP_TCP_PORT
             self.ofp_ssl_listen_port = ofproto_common.OFP_SSL_PORT
-            # For the backward compatibility, we spawn a server loop
-            # listening on the old OpenFlow listen port 6633.
+            # 为了向后兼容，我们产生一个服务器循环
+            # 监听旧的of监听端口6633.
             hub.spawn(self.server_loop,
                       ofproto_common.OFP_TCP_PORT_OLD,
                       ofproto_common.OFP_SSL_PORT_OLD)
@@ -93,12 +93,12 @@ class OpenFlowController(object):
             self.ofp_tcp_listen_port = CONF.ofp_tcp_listen_port
             self.ofp_ssl_listen_port = CONF.ofp_ssl_listen_port
 
-    # entry point
+# 入口点
     def __call__(self):
-        # LOG.debug('call')
+        # LOG.debug('call') 
         self.server_loop(self.ofp_tcp_listen_port,
                          self.ofp_ssl_listen_port)
-
+#服务循环
     def server_loop(self, ofp_tcp_listen_port, ofp_ssl_listen_port):
         if CONF.ctl_privkey is not None and CONF.ctl_cert is not None:
             if CONF.ca_certs is not None:
@@ -125,7 +125,7 @@ class OpenFlowController(object):
         # LOG.debug('loop')
         server.serve_forever()
 
-
+#关闭函数
 def _deactivate(method):
     def deactivate(self):
         try:
@@ -140,58 +140,56 @@ def _deactivate(method):
                 self.socket.close()
     return deactivate
 
-
+#数据路径类
 class Datapath(ofproto_protocol.ProtocolDesc):
     """
-    A class to describe an OpenFlow switch connected to this controller.
+    描述连接到该控制器的OpenFlow开关的类。
 
-    An instance has the following attributes.
+    实例具有以下属性。
 
-    .. tabularcolumns:: |l|L|
+    .. 表格列:: |l|L|
 
     ==================================== ======================================
-    Attribute                            Description
+    属性                                 描述
     ==================================== ======================================
     id                                   64-bit OpenFlow Datapath ID.
-                                         Only available for
+                                         只能应用于
                                          ryu.controller.handler.MAIN_DISPATCHER
                                          phase.
-    ofproto                              A module which exports OpenFlow
-                                         definitions, mainly constants appeared
-                                         in the specification, for the
-                                         negotiated OpenFlow version.  For
-                                         example, ryu.ofproto.ofproto_v1_0 for
-                                         OpenFlow 1.0.
-    ofproto_parser                       A module which exports OpenFlow wire
-                                         message encoder and decoder for the
-                                         negotiated OpenFlow version.
-                                         For example,
-                                         ryu.ofproto.ofproto_v1_0_parser
+                                         
+    ofproto                              导出OpenFlow定义的模块，主要是常量
+                                         出现在规范中，用于协商的OpenFlow版本。
+                                         例如，对于OpenFlow 1.0，ryu.ofproto.ofproto_v1_0。
+                                         
+    ofproto_parser                       导出OpenFlow定义的解析模块，主要是常量
+                                         出现在规范中，用于协商的OpenFlow版本。
+                                         例如,ryu.ofproto.ofproto_v1_0_parser
                                          for OpenFlow 1.0.
-    ofproto_parser.OFPxxxx(datapath,...) A callable to prepare an OpenFlow
-                                         message for the given switch.  It can
-                                         be sent with Datapath.send_msg later.
-                                         xxxx is a name of the message.  For
-                                         example OFPFlowMod for flow-mod
-                                         message.  Arguemnts depend on the
-                                         message.
-    set_xid(self, msg)                   Generate an OpenFlow XID and put it
-                                         in msg.xid.
-    send_msg(self, msg)                  Queue an OpenFlow message to send to
-                                         the corresponding switch.  If msg.xid
-                                         is None, set_xid is automatically
-                                         called on the message before queueing.
-    send_packet_out                      deprecated
-    send_flow_mod                        deprecated
-    send_flow_del                        deprecated
-    send_delete_all_flows                deprecated
-    send_barrier                         Queue an OpenFlow barrier message to
-                                         send to the switch.
-    send_nxt_set_flow_format             deprecated
-    is_reserved_port                     deprecated
+                                         
+    ofproto_parser.OFPxxxx(datapath,...) 可以准备一个OpenFlow
+                                         给定交换机的消息。它可以
+                                         请稍候再与Datapath.send_msg一起发送。
+                                         xxxx是消息的名称。对于
+                                         FlowPF的OFPFlowMod示例
+                                         信息。 Arguemnts依赖于
+                                         信息。
+    set_xid(self, msg)                   生成一个OpenFlow XID 并把它放在msg.xid.
+    
+    send_msg(self, msg)                  队列一个要发送的OpenFlow消息
+                                         相应的开关。如果msg.xid
+                                         是None，set_xid是自动的
+                                         在排队前呼叫消息。
+    send_packet_out                      弃用
+    send_flow_mod                        弃用
+    send_flow_del                        弃用
+    send_delete_all_flows                弃用
+    send_barrier                         将OpenFlow屏障消息排队
+                                         发送到交换机.
+    send_nxt_set_flow_format             弃用
+    is_reserved_port                     弃用
     ==================================== ======================================
     """
-
+#初始化
     def __init__(self, socket, address):
         super(Datapath, self).__init__()
 
@@ -201,8 +199,8 @@ class Datapath(ofproto_protocol.ProtocolDesc):
         self.address = address
         self.is_active = True
 
-        # The limit is arbitrary. We need to limit queue size to
-        # prevent it from eating memory up.
+        #限制是任意的。 我们需要限制队列大小，以防止其记忆。
+        
         self.send_q = hub.Queue(16)
         self._send_q_sem = hub.BoundedSemaphore(self.send_q.maxsize)
 
@@ -211,26 +209,26 @@ class Datapath(ofproto_protocol.ProtocolDesc):
         self.unreplied_echo_requests = []
 
         self.xid = random.randint(0, self.ofproto.MAX_XID)
-        self.id = None  # datapath_id is unknown yet
+        self.id = None  # datapath_id 暂时还不知道
         self._ports = None
         self.flow_format = ofproto_v1_0.NXFF_OPENFLOW10
         self.ofp_brick = ryu.base.app_manager.lookup_service_brick('ofp_event')
         self.set_state(HANDSHAKE_DISPATCHER)
-
+#关闭函数
     @_deactivate
     def close(self):
         if self.state != DEAD_DISPATCHER:
             self.set_state(DEAD_DISPATCHER)
-
+#设置状态
     def set_state(self, state):
         self.state = state
         ev = ofp_event.EventOFPStateChange(self)
         ev.state = state
         self.ofp_brick.send_event_to_observers(ev, state)
 
-    # Low level socket handling layer
+    # 低级套接字处理层
     @_deactivate
-    def _recv_loop(self):
+    def _recv_loop(self):   #接收循环
         buf = bytearray()
         count = 0
         min_read_len = remaining_read_len = ofproto_common.OFP_HEADER_SIZE
@@ -244,8 +242,8 @@ class Datapath(ofproto_protocol.ProtocolDesc):
             except SocketTimeout:
                 continue
             except ssl.SSLError:
-                # eventlet throws SSLError (which is a subclass of IOError)
-                # on SSL socket read timeout; re-try the loop in this case.
+                # eventlet抛出SSL错误(which is a subclass of IOError)
+                # on SSL socket read timeout; 在这个情况下重新尝试循环.
                 continue
             except (EOFError, IOError):
                 break
@@ -258,7 +256,7 @@ class Datapath(ofproto_protocol.ProtocolDesc):
             while buf_len >= min_read_len:
                 (version, msg_type, msg_len, xid) = ofproto_parser.header(buf)
                 if (msg_len < min_read_len):
-                    # Someone isn't playing nicely; log it, and try something sane.
+                    # Someone isn't playing nicely; log it, and try something sane.（理性的东西）
                     LOG.debug("Message with invalid length %s received from switch at address %s",
                               msg_len, self.address)
                     msg_len = min_read_len
@@ -285,14 +283,14 @@ class Datapath(ofproto_protocol.ProtocolDesc):
                 remaining_read_len = min_read_len
 
                 # We need to schedule other greenlets. Otherwise, ryu
-                # can't accept new switches or handle the existing
-                # switches. The limit is arbitrary. We need the better
-                # approach in the future.
+                # 不能接受新的交换机or 处理存在的
+                # 交换机. 限制是任意的. 在未来我们需要
+                # 更好的方法.
                 count += 1
                 if count > 2048:
                     count = 0
                     hub.sleep(0)
-
+#发送循环
     def _send_loop(self):
         try:
             while self.state != DEAD_DISPATCHER:
@@ -300,27 +298,27 @@ class Datapath(ofproto_protocol.ProtocolDesc):
                 self._send_q_sem.release()
                 self.socket.sendall(buf)
         except SocketTimeout:
-            LOG.debug("Socket timed out while sending data to switch at address %s",
+            LOG.debug("Socket timed out while sending data to switch at address %s", #Socket在发送数据时时间到了就切换到某地址
                       self.address)
         except IOError as ioe:
-            # Convert ioe.errno to a string, just in case it was somehow set to None.
+            # 将ioe.errno转换为字符串，以防其以某种方式设置为无。
             errno = "%s" % ioe.errno
             LOG.debug("Socket error while sending data to switch at address %s: [%s] %s",
                       self.address, errno, ioe.strerror)
         finally:
             q = self.send_q
-            # First, clear self.send_q to prevent new references.
+            # 首先，清除self.send_q以防止新引用.
             self.send_q = None
-            # Now, drain the send_q, releasing the associated semaphore for each entry.
-            # This should release all threads waiting to acquire the semaphore.
+            # 现在，排除send_q，为每个条目释放相关的信号量.
+            # 这应该释放所有线程等待获取信号量.
             try:
                 while q.get(block=False):
                     self._send_q_sem.release()
             except hub.QueueEmpty:
                 pass
-            # Finally, ensure the _recv_loop terminates.
+            # 最后，确保_recv_loop终止。
             self.close()
-
+#发送函数
     def send(self, buf):
         msg_enqueued = False
         self._send_q_sem.acquire()
@@ -333,13 +331,13 @@ class Datapath(ofproto_protocol.ProtocolDesc):
             LOG.debug('Datapath in process of terminating; send() to %s discarded.',
                       self.address)
         return msg_enqueued
-
+#设置交换机标识
     def set_xid(self, msg):
         self.xid += 1
         self.xid &= self.ofproto.MAX_XID
         msg.set_xid(self.xid)
         return self.xid
-
+#发送信息
     def send_msg(self, msg):
         assert isinstance(msg, self.ofproto_parser.MsgBase)
         if msg.xid is None:
@@ -347,7 +345,7 @@ class Datapath(ofproto_protocol.ProtocolDesc):
         msg.serialize()
         # LOG.debug('send_msg %s', msg)
         return self.send(msg.buf)
-
+#echo请求循环
     def _echo_request_loop(self):
         if not self.max_unreplied_echo_requests:
             return
@@ -358,17 +356,17 @@ class Datapath(ofproto_protocol.ProtocolDesc):
             self.send_msg(echo_req)
             hub.sleep(self.echo_request_interval)
         self.close()
-
+#告知已收到echo回复
     def acknowledge_echo_reply(self, xid):
         try:
             self.unreplied_echo_requests.remove(xid)
         except:
             pass
-
+#服务
     def serve(self):
         send_thr = hub.spawn(self._send_loop)
 
-        # send hello message immediately
+        # 立即发送hello消息
         hello = self.ofproto_parser.OFPHello(self)
         self.send_msg(hello)
 
@@ -382,9 +380,9 @@ class Datapath(ofproto_protocol.ProtocolDesc):
             hub.joinall([send_thr, echo_thr])
             self.is_active = False
 
-    #
-    # Utility methods for convenience
-    #
+#
+# 实用方法 for convenience
+#发出数据包
     def send_packet_out(self, buffer_id=0xffffffff, in_port=None,
                         actions=None, data=None):
         if in_port is None:
@@ -392,7 +390,7 @@ class Datapath(ofproto_protocol.ProtocolDesc):
         packet_out = self.ofproto_parser.OFPPacketOut(
             self, buffer_id, in_port, actions, data)
         self.send_msg(packet_out)
-
+#流发送模块
     def send_flow_mod(self, rule, cookie, command, idle_timeout, hard_timeout,
                       priority=None, buffer_id=0xffffffff,
                       out_port=None, flags=0, actions=None):
@@ -416,53 +414,53 @@ class Datapath(ofproto_protocol.ProtocolDesc):
                 self, cookie, command, idle_timeout, hard_timeout,
                 priority, buffer_id, out_port, flags, rule, actions)
         self.send_msg(flow_mod)
-
+#流发送删除
     def send_flow_del(self, rule, cookie, out_port=None):
         self.send_flow_mod(rule=rule, cookie=cookie,
                            command=self.ofproto.OFPFC_DELETE,
                            idle_timeout=0, hard_timeout=0, priority=0,
                            out_port=out_port)
-
+#发出删除所有流
     def send_delete_all_flows(self):
         rule = nx_match.ClsRule()
         self.send_flow_mod(
             rule=rule, cookie=0, command=self.ofproto.OFPFC_DELETE,
             idle_timeout=0, hard_timeout=0, priority=0, buffer_id=0,
             out_port=self.ofproto.OFPP_NONE, flags=0, actions=None)
-
+#发送屏障
     def send_barrier(self):
         barrier_request = self.ofproto_parser.OFPBarrierRequest(self)
         return self.send_msg(barrier_request)
-
+#发送下一个流设置格式
     def send_nxt_set_flow_format(self, flow_format):
         assert (flow_format == ofproto_v1_0.NXFF_OPENFLOW10 or
                 flow_format == ofproto_v1_0.NXFF_NXM)
         if self.flow_format == flow_format:
-            # Nothing to do
+            # 什么也不做
             return
         self.flow_format = flow_format
         set_format = self.ofproto_parser.NXTSetFlowFormat(self, flow_format)
         # FIXME: If NXT_SET_FLOW_FORMAT or NXFF_NXM is not supported by
-        # the switch then an error message will be received. It may be
+        # the switch then 收到一个错误消息. 它可能是
         # handled by setting self.flow_format to
         # ofproto_v1_0.NXFF_OPENFLOW10 but currently isn't.
         self.send_msg(set_format)
         self.send_barrier()
-
+#保留端口
     def is_reserved_port(self, port_no):
         return port_no > self.ofproto.OFPP_MAX
 
-
+#数据路径连接factory
 def datapath_connection_factory(socket, address):
     LOG.debug('connected socket:%s address:%s', socket, address)
     with contextlib.closing(Datapath(socket, address)) as datapath:
         try:
             datapath.serve()
         except:
-            # Something went wrong.
-            # Especially malicious switch can send malformed packet,
-            # the parser raise exception.
-            # Can we do anything more graceful?
+            # 出了些问题.
+            # 特别是恶意切换可能会发送格式错误的数据包,
+            # 解析器引发异常.
+            # 我们可以做任何更优雅的事情吗?
             if datapath.id is None:
                 dpid_str = "%s" % datapath.id
             else:
