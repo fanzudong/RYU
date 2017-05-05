bdac150cdb6f6a8c02921a484034057bb9ceaf63
fanzudong <1179528188@qq.com>
2017-05-05 15:50:58 +0800

    Update handler.py

--- controller/handler.py
+++ controller/handler.py
@@ -20,24 +20,24 @@ import sys
 
 LOG = logging.getLogger('ryu.controller.handler')
 
-# just represent OF datapath state. datapath specific so should be moved.
+# 仅代表数据通路状态。 datapath具体所以应该移动。
 HANDSHAKE_DISPATCHER = "handshake"
 CONFIG_DISPATCHER = "config"
 MAIN_DISPATCHER = "main"
 DEAD_DISPATCHER = "dead"
 
-
+#调用函数类
 class _Caller(object):
     """Describe how to handle an event class.
     """
 
     def __init__(self, dispatchers, ev_source):
-        """Initialize _Caller.
+        """初始化 _Caller.
 
-        :param dispatchers: A list of states or a state, in which this
-                            is in effect.
+        :param dispatchers: 一个生效的状态列表
                             None and [] mean all states.
-        :param ev_source: The module which generates the event.
+                            
+        :param ev_source: 产生事件的模块.
                           ev_cls.__module__ for set_ev_cls.
                           None for set_ev_handler.
         """
@@ -45,35 +45,34 @@ class _Caller(object):
         self.ev_source = ev_source
 
 
-# should be named something like 'observe_event'
+# 应该被命名为 '监听事件'
 def set_ev_cls(ev_cls, dispatchers=None):
     """
-    A decorator for Ryu application to declare an event handler.
+    Ryu应用程序的装饰器声明一个事件处理程序。
 
-    Decorated method will become an event handler.
-    ev_cls is an event class whose instances this RyuApp wants to receive.
-    dispatchers argument specifies one of the following negotiation phases
-    (or a list of them) for which events should be generated for this handler.
-    Note that, in case an event changes the phase, the phase before the change
-    is used to check the interest.
+    装饰的方法将成为一个事件处理程序。
+    ev_cls是这个RyuApp想要接收的实例的事件类。
+    dispatchers参数指定以下协商阶段之一
+   （或它们的列表），为此处理程序应为其生成事件。
+    请注意，如果事件改变阶段，则更改前的阶段
+    用于检查兴趣。
 
-    .. tabularcolumns:: |l|L|
+    .. 表格列:: |l|L|
 
     =========================================== ===============================
-    Negotiation phase                           Description
+    协商阶段                                    描述
     =========================================== ===============================
-    ryu.controller.handler.HANDSHAKE_DISPATCHER Sending and waiting for hello
-                                                message
-    ryu.controller.handler.CONFIG_DISPATCHER    Version negotiated and sent
+    ryu.controller.handler.HANDSHAKE_DISPATCHER 发送和等待hello消息
+    ryu.controller.handler.CONFIG_DISPATCHER    版本协商 and sent
                                                 features-request message
     ryu.controller.handler.MAIN_DISPATCHER      Switch-features message
-                                                received and sent set-config
-                                                message
-    ryu.controller.handler.DEAD_DISPATCHER      Disconnect from the peer.  Or
-                                                disconnecting due to some
-                                                unrecoverable errors.
+                                                接收和发送设置配置信息
+    ryu.controller.handler.DEAD_DISPATCHER      断开与对端的连接。 要么
+                                                断开由于某些不可恢复
+                                                的错误的连接
     =========================================== ===============================
     """
+#设置事件类调度者
     def _set_ev_cls_dec(handler):
         if 'callers' not in dir(handler):
             handler.callers = {}
@@ -82,7 +81,7 @@ def set_ev_cls(ev_cls, dispatchers=None):
         return handler
     return _set_ev_cls_dec
 
-
+#设置事件处理
 def set_ev_handler(ev_cls, dispatchers=None):
     def _set_ev_cls_dec(handler):
         if 'callers' not in dir(handler):
@@ -92,11 +91,11 @@ def set_ev_handler(ev_cls, dispatchers=None):
         return handler
     return _set_ev_cls_dec
 
-
+#有调用者
 def _has_caller(meth):
     return hasattr(meth, 'callers')
 
-
+#列表化
 def _listify(may_list):
     if may_list is None:
         may_list = []
@@ -104,7 +103,7 @@ def _listify(may_list):
         may_list = [may_list]
     return may_list
 
-
+#注册实例
 def register_instance(i):
     for _k, m in inspect.getmembers(i, inspect.ismethod):
         # LOG.debug('instance %s k %s m %s', i, _k, m)
@@ -112,11 +111,11 @@ def register_instance(i):
             for ev_cls, c in m.callers.items():
                 i.register_handler(ev_cls, m)
 
-
+#方法
 def _is_method(f):
     return inspect.isfunction(f) or inspect.ismethod(f)
 
-
+#获取依赖服务
 def get_dependent_services(cls):
     services = []
     for _k, m in inspect.getmembers(cls, _is_method):
@@ -125,8 +124,8 @@ def get_dependent_services(cls):
                 service = getattr(sys.modules[ev_cls.__module__],
                                   '_SERVICE_NAME', None)
                 if service:
-                    # avoid cls that registers the own events (like
-                    # ofp_handler)
+                    # 避免注册自己的事件的类
+                    # 像ofp_handler
                     if cls.__module__ != service:
                         services.append(service)
 
@@ -135,18 +134,18 @@ def get_dependent_services(cls):
     services = list(set(services))
     return services
 
-
+#注册服务
 def register_service(service):
     """
-    Register the ryu application specified by 'service' as
-    a provider of events defined in the calling module.
+    将“service”指定的ryu应用程序注册为
+    在调用模块中定义的事件提供者。
 
-    If an application being loaded consumes events (in the sense of
-    set_ev_cls) provided by the 'service' application, the latter
-    application will be automatically loaded.
+    如果正在加载的应用程序消耗事件（在某种意义上）
+    set_ev_cls）由'service'应用程序提供，后者
+    应用程序将自动加载。
 
-    This mechanism is used to e.g. automatically start ofp_handler if
-    there are applications consuming OFP events.
+    该机制用于例如 如果自动启动p_handler
+    有消耗OFP事件的应用程序。
     """
     frame = inspect.currentframe()
     m_name = frame.f_back.f_globals['__name__']
