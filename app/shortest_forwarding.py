41494813b5580680f12bb8672a68170e30dbe54d
fanzudong <1179528188@qq.com>
2017-05-08 16:04:49 +0800

    Update shortest_forwarding.py

--- app/shortest_forwarding.py
+++ app/shortest_forwarding.py
@@ -41,24 +41,21 @@ import network_delay_detector
 
 CONF = cfg.CONF
 
-
+#最短路径转发类
 class ShortestForwarding(app_manager.RyuApp):
     """
-        ShortestForwarding is a Ryu app for forwarding packets in shortest
-        path.
-        This App does not defined the path computation method.
-        To get shortest path, this module depends on network awareness,
-        network monitor and network delay detecttor modules.
+        最短的转发是用于在最短路径中转发数据包的Ryu应用程序，此应用程序未定义路径计算方法。
+        要获得最短路径，该模块取决于网络感知，网络监控和网络延迟检测模块。
     """
-
+#openflow1.3
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
     _CONTEXTS = {
         "network_awareness": network_awareness.NetworkAwareness,
         "network_monitor": network_monitor.NetworkMonitor,
         "network_delay_detector": network_delay_detector.NetworkDelayDetector}
-
+#权重模型
     WEIGHT_MODEL = {'hop': 'weight', 'delay': "delay", "bw": "bw"}
-
+#初始化
     def __init__(self, *args, **kwargs):
         super(ShortestForwarding, self).__init__(*args, **kwargs)
         self.name = 'shortest_forwarding'
@@ -67,21 +64,21 @@ class ShortestForwarding(app_manager.RyuApp):
         self.delay_detector = kwargs["network_delay_detector"]
         self.datapaths = {}
         self.weight = self.WEIGHT_MODEL[CONF.weight]
-
+#设置权重模块
     def set_weight_mode(self, weight):
         """
-            set weight mode of path calculating.
+            设置路径计算的权重模式
         """
         self.weight = weight
         if self.weight == self.WEIGHT_MODEL['hop']:
             self.awareness.get_shortest_paths(weight=self.weight)
         return True
-
+#状态改变处理
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         """
-            Collect datapath information.
+            收集datapath信息.
         """
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
@@ -92,10 +89,10 @@ class ShortestForwarding(app_manager.RyuApp):
             if datapath.id in self.datapaths:
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
-
+#增加流
     def add_flow(self, dp, p, match, actions, idle_timeout=0, hard_timeout=0):
         """
-            Send a flow entry to datapath.
+           给datapath发送一个流.
         """
         ofproto = dp.ofproto
         parser = dp.ofproto_parser
@@ -108,10 +105,10 @@ class ShortestForwarding(app_manager.RyuApp):
                                 hard_timeout=hard_timeout,
                                 match=match, instructions=inst)
         dp.send_msg(mod)
-
+发送流mod
     def send_flow_mod(self, datapath, flow_info, src_port, dst_port):
         """
-            Build flow entry, and send it to datapath.
+           构建流入口，并将其发送到datapath
         """
         parser = datapath.ofproto_parser
         actions = []
@@ -123,10 +120,10 @@ class ShortestForwarding(app_manager.RyuApp):
 
         self.add_flow(datapath, 1, match, actions,
                       idle_timeout=15, hard_timeout=60)
-
+#构建数据包输出
     def _build_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
         """
-            Build packet out object.
+            构建数据包输出对象
         """
         actions = []
         if dst_port:
@@ -142,19 +139,19 @@ class ShortestForwarding(app_manager.RyuApp):
             datapath=datapath, buffer_id=buffer_id,
             data=msg_data, in_port=src_port, actions=actions)
         return out
-
+#发出数据包
     def send_packet_out(self, datapath, buffer_id, src_port, dst_port, data):
         """
-            Send packet out packet to assigned datapath.
+            发送数据包到分配的数据路径
         """
         out = self._build_packet_out(datapath, buffer_id,
                                      src_port, dst_port, data)
         if out:
             datapath.send_msg(out)
-
+#获取端口
     def get_port(self, dst_ip, access_table):
         """
-            Get access port if dst host.
+            获取接入端口 if dst host.
             access_table: {(sw,port) :(ip, mac)}
         """
         if access_table:
@@ -164,10 +161,10 @@ class ShortestForwarding(app_manager.RyuApp):
                         dst_port = key[1]
                         return dst_port
         return None
-
+#从链路获取端口对
     def get_port_pair_from_link(self, link_to_port, src_dpid, dst_dpid):
         """
-            Get port pair of link, so that controller can install flow entry.
+            获取端口对的链接，使控制器可以安装流入
         """
         if (src_dpid, dst_dpid) in link_to_port:
             return link_to_port[(src_dpid, dst_dpid)]
@@ -175,11 +172,10 @@ class ShortestForwarding(app_manager.RyuApp):
             self.logger.info("dpid:%s->dpid:%s is not in links" % (
                              src_dpid, dst_dpid))
             return None
-
+#流
     def flood(self, msg):
         """
-            Flood ARP packet to the access port
-            which has no record of host.
+            流ARP数据包到没有记录的接入端口
         """
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -194,11 +190,10 @@ class ShortestForwarding(app_manager.RyuApp):
                         ofproto.OFPP_CONTROLLER, port, msg.data)
                     datapath.send_msg(out)
         self.logger.debug("Flooding msg")
-
+#地址解析协议转发
     def arp_forwarding(self, msg, src_ip, dst_ip):
-        """ Send ARP packet to the destination host,
-            if the dst host record is existed,
-            else, flow it to the unknow access port.
+        """
+        将ARP数据包发送到目标主机，如果存在dst主机记录，否则将其传送到未知访问端口。
         """
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -215,10 +210,10 @@ class ShortestForwarding(app_manager.RyuApp):
             self.logger.debug("Reply ARP to knew host")
         else:
             self.flood(msg)
-
+#获取路径
     def get_path(self, src, dst, weight):
         """
-            Get shortest path from network awareness module.
+            从网络感知模块获取最短路径
         """
         shortest_paths = self.awareness.shortest_paths
         graph = self.awareness.graph
@@ -226,7 +221,7 @@ class ShortestForwarding(app_manager.RyuApp):
         if weight == self.WEIGHT_MODEL['hop']:
             return shortest_paths.get(src).get(dst)[0]
         elif weight == self.WEIGHT_MODEL['delay']:
-            # If paths existed, return it, else calculate it and save it.
+            # 如果存在路径，返回它，否则计算并保存
             try:
                 paths = shortest_paths.get(src).get(dst)
                 return paths[0]
@@ -238,25 +233,23 @@ class ShortestForwarding(app_manager.RyuApp):
                 shortest_paths[src].setdefault(dst, paths)
                 return paths[0]
         elif weight == self.WEIGHT_MODEL['bw']:
-            # Because all paths will be calculate
-            # when call self.monitor.get_best_path_by_bw
-            # So we just need to call it once in a period,
-            # and then, we can get path directly.
+            # 因为所有的路径都会计算在调用self.monitor.get_best_path_by_bw
+            # 所以我们只需要在一段时间内调用一次，然后我们可以直接获取路径。
             try:
-                # if path is existed, return it.
+                # 如果路径存在，返回它
                 path = self.monitor.best_paths.get(src).get(dst)
                 return path
             except:
-                # else, calculate it, and return.
+                # 否则，计算它，并返回它
                 result = self.monitor.get_best_path_by_bw(graph,
                                                           shortest_paths)
                 paths = result[1]
                 best_path = paths.get(src).get(dst)
                 return best_path
-
+#获取交换机
     def get_sw(self, dpid, in_port, src, dst):
         """
-            Get pair of source and destination switches.
+            获取一对源和目的地交换机
         """
         src_sw = dpid
         dst_sw = None
@@ -273,11 +266,11 @@ class ShortestForwarding(app_manager.RyuApp):
             dst_sw = dst_location[0]
 
         return src_sw, dst_sw
-
+#安装流
     def install_flow(self, datapaths, link_to_port, access_table, path,
                      flow_info, buffer_id, data=None):
         ''' 
-            Install flow entires for roundtrip: go and back.
+            安装往返的流条目：往返
             @parameter: path=[dpid1, dpid2...]
                         flow_info=(eth_type, src_ip, dst_ip, in_port)
         '''
@@ -320,7 +313,7 @@ class ShortestForwarding(app_manager.RyuApp):
             self.send_flow_mod(last_dp, flow_info, src_port, dst_port)
             self.send_flow_mod(last_dp, back_info, dst_port, src_port)
 
-            # the first flow entry
+            # 第一个流进入
             port_pair = self.get_port_pair_from_link(link_to_port,
                                                      path[0], path[1])
             if port_pair is None:
@@ -331,7 +324,7 @@ class ShortestForwarding(app_manager.RyuApp):
             self.send_flow_mod(first_dp, back_info, out_port, in_port)
             self.send_packet_out(first_dp, buffer_id, in_port, out_port, data)
 
-        # src and dst on the same datapath
+        # 源和目的在同一路径
         else:
             out_port = self.get_port(flow_info[2], access_table)
             if out_port is None:
@@ -340,10 +333,10 @@ class ShortestForwarding(app_manager.RyuApp):
             self.send_flow_mod(first_dp, flow_info, in_port, out_port)
             self.send_flow_mod(first_dp, back_info, out_port, in_port)
             self.send_packet_out(first_dp, buffer_id, in_port, out_port, data)
-
+#最短路径转发
     def shortest_forwarding(self, msg, eth_type, ip_src, ip_dst):
         """
-            To calculate shortest forwarding path and install them into datapaths.
+            计算最短转发路径并将其安装到数据路径中
         """
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -354,22 +347,21 @@ class ShortestForwarding(app_manager.RyuApp):
         if result:
             src_sw, dst_sw = result[0], result[1]
             if dst_sw:
-                # Path has already calculated, just get it.
+                # 路径已经计算好了，仅需得到它.
                 path = self.get_path(src_sw, dst_sw, weight=self.weight)
                 self.logger.info("[PATH]%s<-->%s: %s" % (ip_src, ip_dst, path))
                 flow_info = (eth_type, ip_src, ip_dst, in_port)
-                # install flow entries to datapath along side the path.
+                # 将流条目安装到路径旁的数据路径
                 self.install_flow(self.datapaths,
                                   self.awareness.link_to_port,
                                   self.awareness.access_table, path,
                                   flow_info, msg.buffer_id, msg.data)
         return
-
+#数据包进入处理
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         '''
-            In packet_in handler, we need to learn access_table by ARP.
-            Therefore, the first packet from UNKOWN host MUST be ARP.
+           在packet_in处理程序中，我们需要通过ARP学习access_table。 因此，未知主机的第一个包必须是ARP。
         '''
         msg = ev.msg
         datapath = msg.datapath
